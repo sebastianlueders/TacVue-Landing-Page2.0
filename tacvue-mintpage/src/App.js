a774d582949245625ddbba6e9e515d0ecd091dd8
Seb Lueders <38769233+sebastianlueders@users.noreply.github.com>
2022-09-15 19:25:45 -0500

    added contact nav and pages

--- tacvue-mintpage/src/App.js
+++ tacvue-mintpage/src/App.js
@@ -1,5 +1,18 @@
 import logo from './tacvue-white-icon (2).svg';
 import './App.css';
+import email from './mail.svg'
+import telegram from './telegram.svg'
+import tiktok from './tiktok.svg'
+import twitter from './twitter.svg'
+import youtube from './youtube.svg'
+import { isMobile } from "react-device-detect";
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+
+const handleClick1 = () => {
+  window.open("https://app.gitbook.com/o/1td89P74iu3wc47FF65p/s/RvDdaP4zFfYAJOkRacUk/~/changes/B44Uu6ecW43ZPMDXqZx9/", "_blank");
+}
+
 
 function App() {
   return (
@@ -12,21 +25,21 @@ function App() {
         </header>
 
         <main class="container" role="main">
-          <article class="content" role="sections">
-            <div class="panel">
+          <article class="content">
+            <div class="panel" Link href="/early-access.js">
               <div class="descriptor">EARLY<br/>ACCESS</div>
             </div>
-            <div class="panel">
+            <div class="panel" Link href="/web3-services.js">
               <div class="descriptor">WEB3<br/>SERVICES</div>
             </div>
-            <div class="panel">
+            <div class="panel" Link href="/multiverse-entities.js">
               <div class="descriptor">MULTIVERSE<br/>ENTITIES</div>
             </div>
-            <div class="panel">
+            <div class="panel" Link href="/brand-blueprint.js">
               <div class="descriptor">BRAND<br/>BLUEPRINT</div>
             </div>
-            <div class="panel">
-              <div class="descriptor">LEARN<br/>MORE</div>
+            <div class="panel" onClick={handleClick1}>
+              <div class="descriptor"> LEARN<br/>MORE</div>
             </div>
           </article>
 
@@ -43,19 +56,32 @@ function App() {
           </div>
         </main>
 
-        <footer class="main-footer" role="content-info">
+        <footer class="main-footer">
 
-          <nav>
-            <h3>Mail</h3>
-            <h3>Telegram</h3>
-            <h3>Tik Tok</h3>
-            <h3>Twitter</h3>
-            <h3>Blog</h3>
-          </nav>
+          <table class="contact">
+            <tr>
+              <td>
+                <img src={email} alt="Email us" height="30px" class="contact" id='mail'></img>
+              </td>
+              <td>
+                <img src={telegram} alt="Telegram us" height="30px" class="contact" id='telegram'></img>
+              </td>
+              <td>
+                <img src={tiktok} alt="Follow us on Tik Tok" height="30px" class="contact" id='tik-tok'></img>
+              </td>
+              <td>
+                <img src={twitter} alt="Follow us on Twitter" height="30px" class="contact" id='twitter'></img>
+              </td>
+              <td>
+                <img src={youtube} alt="Follow us on YouTube" height="30px" class="contact" id='youtube'></img>
+              </td>
+            </tr>
+          </table>
     
           <div class="legal">
-            <p>Terms of Use</p>
-            <p>Privacy Policy</p>
+            <a Link href='./terms-of-use.js'>Terms of Use</a>
+            <br></br>
+            <a Link href='./privacy-policy.js'>Privacy Policy</a>
           </div>
 
           <div class="ownership">
